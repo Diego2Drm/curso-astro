5afe3b8261f42e28ef86f7d2dd3de3ca26cfbbaa
Diego Ramirez <diego.ramirez2d03@gmail.com>
2026-01-07 20:52:49 -0600

    midu-dev-books: Server Islands

--- midudev/dev-books/astro.config.mjs
+++ midudev/dev-books/astro.config.mjs
@@ -2,16 +2,19 @@
 import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  output: 'server',
+  adapter: netlify(),
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public' }),
-    SCORE_API_ENDPOINT: envField.string({ context: 'client', access: 'public' })
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
     }
   }
 });
